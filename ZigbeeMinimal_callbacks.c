c00795f60e982868ca33c7f4562d35d344f31131
imacht <94927586+imacht@users.noreply.github.com>
2022-02-16 16:44:34 +0000

    Add some init/reboot states and reboot-on-network-loss

--- ZigbeeMinimal_callbacks.c
+++ ZigbeeMinimal_callbacks.c
@@ -1,37 +1,29 @@
-/***************************************************************************//**
+/*******************************************************************************
  * @file
- * @brief Callback implementation for ZigbeeMinimal sample application.
+ * @brief Initiated from ZigbeeMinimal sample application.
  *******************************************************************************
- * # License
- * <b>Copyright 2019 Silicon Laboratories Inc. www.silabs.com</b>
- *******************************************************************************
- *
- * The licensor of this software is Silicon Laboratories Inc. Your use of this
- * software is governed by the terms of Silicon Labs Master Software License
- * Agreement (MSLA) available at
- * www.silabs.com/about-us/legal/master-software-license-agreement. This
- * software is distributed to you in Source Code format and is governed by the
- * sections of the MSLA applicable to Source Code.
  *
  ******************************************************************************/
 
-// This callback file is created for your convenience. You may add application
-// code to this file. If you regenerate this file over a previous version, the
-// previous version will be overwritten and any code you have added will be
-// lost.
-
 #include "app/framework/include/af.h"
 #include "network-find.h"
+#include "ember-types.h"
 //#include "update-tc-link-key.h"
 #include "zmeter/zmeter.h"
 #include "gubbins.h"
 
 #define ROUTER_ENDPOINT (1)
+#define COORD_ENDPOINT  (1)
 
-#define ALL_CHANNELS_MASK       0x07FFF800
-#define INIT_KE_DELAY_MS        1000
-#define BUTTON0         0
-#define BUTTON1         1
+#define ALL_CHANNELS_MASK       (0x07FFF800)
+#define COMMISSIONING_DELAY_MS  (5000)
+#define INIT_KE_DELAY_MS        (1000)
+#define BUTTON0                 (0)
+#define BUTTON1                 (1)
+
+// flag bits
+#define COMMISSIONED	(1)
+
 
 static sl_zigbee_event_t commissioning_event;
 static sl_zigbee_event_t init_ke_event;
@@ -43,11 +35,17 @@ static sl_zigbee_event_t find_services_event;
 static struct cluster *aclust;
 
 typedef enum {
+    UNINITIALISED,
+    STOPPING,
+    REBOOT,
+    SCANNING,
+    START_KE,
     FIND_METERS,
     FIND_PRICES,
 //TODO:    FIND_OTA,
-} DiscoStage_t;
-static DiscoStage_t findState;
+} JoinStage_t;
+static JoinStage_t r_state = UNINITIALISED;
+static uint8_t flags = 0;
 
 static void find_result(const EmberAfServiceDiscoveryResult *r);
 
@@ -56,13 +54,25 @@ void commissioningEventHandler(SLXU_UC_EVENT)
     EmberStatus status;
 
     slxu_zigbee_event_set_inactive(commissioningEvent);
+    
+    r_state = SCANNING;
 
     // allow joining on all possible channels
     // TODO: might be quicker to search preferred first, investigate?
     status = emberAfSetFormAndJoinChannelMask(0, ALL_CHANNELS_MASK);
     emberAfCorePrintln("------- SetFormAndJoinChannelMask returned : 0x%X", status);
-    status = emberAfStartSearchForJoinableNetwork();
-    emberAfCorePrintln("------- StartSearchForJoinableNetwork returned : 0x%X", status);
+    EmberNetworkInitStruct init_struct = { EMBER_NETWORK_INIT_PARENT_INFO_IN_TOKEN };
+   	status = emberNetworkInit(&init_struct);
+    
+    if (EMBER_NOT_JOINED == status) {
+        emberAfCorePrintln("------- NetworkInit returned EMBER_NOT_JOINED, searching for network...");
+        status = emberAfStartSearchForJoinableNetwork();
+        emberAfCorePrintln("------- StartSearchForJoinableNetwork returned : 0x%X", status);
+    }
+    else if (status)        
+        emberAfCorePrintln("------- NetworkInit returned  0x%X", status);
+    else 
+        emberAfCorePrintln("------- NetworkInit returned Success");
 }
 
 void initKeEventHandler(SLXU_UC_EVENT)
@@ -71,7 +81,7 @@ void initKeEventHandler(SLXU_UC_EVENT)
 
     slxu_zigbee_event_set_inactive(initKeEvent);
 
-    status = emberAfInitiateKeyEstablishment(0x0000,1);
+    status = emberAfInitiateKeyEstablishment(0x0000, COORD_ENDPOINT);
     emberAfCorePrintln("------- InitiateKeyEstablishment returned : 0x%X", status);
 }
 
@@ -86,7 +96,8 @@ boolean emberAfPluginKeyEstablishmentEventCallback(EmberAfKeyEstablishmentNotify
     if (LINK_KEY_ESTABLISHED == status)
     {
         emberAfCorePrintln("------- LINK_KEY_ESTABLISHED ------");
-        findState = FIND_METERS;
+        flags |= COMMISSIONED;
+        r_state = FIND_METERS;
         slxu_zigbee_event_set_delay_ms(findSvcsEvent, 100);
     }
     return true;
@@ -98,7 +109,7 @@ void findServicesHandler()
     uint16_t clust_id = 0;
 
     slxu_zigbee_event_set_inactive(findSvcsEvent);
-    switch (findState) {
+    switch (r_state) {
      case FIND_METERS:
          clust_id = ZCL_SIMPLE_METERING_CLUSTER_ID; break;
      case FIND_PRICES:
@@ -116,19 +127,19 @@ void findServicesHandler()
 static void find_result(const EmberAfServiceDiscoveryResult *r)
 {
     char label[20];
-	uint16_t cluster_id;
+    uint16_t cluster_id;
 
-	if (r->zdoRequestClusterId != MATCH_DESCRIPTORS_REQUEST || r->matchAddress == emberAfGetNodeId())
-		return;
+    if (r->zdoRequestClusterId != MATCH_DESCRIPTORS_REQUEST || r->matchAddress == emberAfGetNodeId())
+        return;
 
-    switch (findState) {
+    switch (r_state) {
      case FIND_METERS:  strcpy(label, "Meter"); cluster_id = ZCL_SIMPLE_METERING_CLUSTER_ID; break;
      case FIND_PRICES:  strcpy(label, "Price"); cluster_id = ZCL_PRICE_CLUSTER_ID; break;
      default: ;
     }
     switch (r->status) {
         case EMBER_AF_BROADCAST_SERVICE_DISCOVERY_COMPLETE:
-            emberAfCorePrintln("------- %s Find result status = EMBER_AF_BROADCAST_SERVICE_DISCOVERY_COMPLETE", label);
+            emberAfCorePrintln("------- %s Find result status = EMBER_AF_BROADCAST_SERVICE_DISCOVERY_COMPLETE\n", label);
             break;
         case EMBER_AF_BROADCAST_SERVICE_DISCOVERY_RESPONSE_RECEIVED  :
             emberAfCorePrintln("------- %s Find result status = EMBER_AF_BROADCAST_SERVICE_DISCOVERY_RESPONSE_RECEIVED", label);
@@ -144,13 +155,13 @@ static void find_result(const EmberAfServiceDiscoveryResult *r)
             return;
     }
     if (emberAfHaveDiscoveryResponseStatus(r->status)) {
-		const EmberAfEndpointList* l = r->responseData;
-//		  struct cluster* nc = cluster_next_circular(aclust);
-//        emberAfCorePrintln("------- %s Find cluster#0: addr: 0x%02X", nc);
-		endpoint_add_new(r->matchAddress, l->list, l->count, cluster_id);
-//		  if (nc) aclust = nc;
-//  	  nc = cluster_next_circular(aclust);
-//        emberAfCorePrintln("------- %s Find cluster#1: addr: 0x%02X", nc);
+        const EmberAfEndpointList* l = r->responseData;
+        struct cluster* nc = cluster_next_circular(aclust);
+        endpoint_add_new(r->matchAddress, l->list, l->count, cluster_id);
+        if (nc) aclust = nc;
+        nc = cluster_next_circular(aclust);
+      emberAfCorePrintln("------- %s Find cluster#1: addr: 0x%02X", label, nc);
+      if (nc) emberAfCorePrintln("-------    cluster#1->next:  0x%02X", nc->next);
 
         emberAfCorePrintln("------- %s Find result: addr: 0x%02X  count: %d",
                                    label, r->matchAddress, l->count);
@@ -159,9 +170,9 @@ static void find_result(const EmberAfServiceDiscoveryResult *r)
     }
     if (EMBER_AF_BROADCAST_SERVICE_DISCOVERY_RESPONSE_RECEIVED != r->status) {
         // Discovery complete
-        if (FIND_METERS == findState)
+        if (FIND_METERS == r_state)
         {
-            findState +=1;
+            r_state +=1;
             slxu_zigbee_event_set_delay_ms(findSvcsEvent, 100);
         }
     }
@@ -172,6 +183,21 @@ void emberAfMainInitCallback(void)
     slxu_zigbee_event_init(commissioningEvent, commissioningEventHandler);
     slxu_zigbee_event_init(initKeEvent, initKeEventHandler);
     slxu_zigbee_event_init(findSvcsEvent, findServicesHandler);
+    
+    slxu_zigbee_event_set_delay_ms(commissioningEvent, COMMISSIONING_DELAY_MS);
+}
+
+void emberAfMainTickCallback(void)
+{
+    if (STOPPING == r_state) {
+		if (!emberPendingAckedMessages() && emberOkToNap()) {
+			emberStackPowerDown();
+			r_state = REBOOT;
+		}
+	} else if (REBOOT == r_state) {
+		//if (emberSerialWriteUsed(APP_SERIAL) == 0)
+			halReboot();
+	}
 }
 
 /** @brief Stack Status
@@ -186,17 +212,38 @@ void emberAfMainInitCallback(void)
  */
 void emberAfStackStatusCallback(EmberStatus status)
 {
-    if (EMBER_NETWORK_UP == status) {
+	if (UNINITIALISED == r_state)   // ignore early bollocks reports
+		return;
+        
+	emberAfCorePrintln("------  StackStatus = 0x%02X,  NetworkState = %d\n", status, emberAfNetworkState());
+
+	if (EMBER_NETWORK_UP != status && EMBER_TRUST_CENTER_EUI_HAS_CHANGED != status)
+		if (flags & COMMISSIONED && emberAfNetworkState() == EMBER_NO_NETWORK) {
+            flags &= ~COMMISSIONED;
+            emberAfCorePrintln("------  StackStatusCb  doing loss-of-network reboot");            
+			r_state = STOPPING;
+        }
+    
+    if (EMBER_NETWORK_UP == status && SCANNING == r_state) {
+        r_state = START_KE;
         slxu_zigbee_event_set_active(initKeEvent);
     }
 }
 
-void emberAfRegistrationCallback(boolean success)
+void emberAfPluginNetworkFindFinishedCallback(EmberStatus status)
 {
-    char *val_str = success?"Success":"Fail";
-    emberAfCorePrintln("------- Registration Complete : %s", val_str);
+	emberAfCorePrintln("------- PluginNetworkFindFinishedCallback status : 0x%02X", status);
+	if (EMBER_NO_BEACONS == status && SCANNING == r_state) {
+		// set BACKING_OFF - 
+	}
 }
 
+bool emberAfPluginNetworkFindJoinCallback(EmberZigbeeNetwork *n, uint8_t lqi, int8_t rssi)
+{
+	emberAfCorePrintln("------- PluginNetworkFindFinishedCallback nwk found chan: %d  panId: 0x%02X  lqi:%d  rssi: %d\n", 
+                    n->channel, n->panId, lqi, rssi);
+	return true;
+}
 
 /** @brief Hal Button Isr
  *
@@ -214,7 +261,7 @@ void emberAfRegistrationCallback(boolean success)
 void sl_button_on_change(const sl_button_t *handle)
 {
   if (SL_SIMPLE_BUTTON_INSTANCE(BUTTON0) == handle) {
-    if ( sl_button_get_state(handle) == SL_SIMPLE_BUTTON_RELEASED) {
+    if (SL_SIMPLE_BUTTON_RELEASED == sl_button_get_state(handle)) {
       slxu_zigbee_event_set_active(commissioningEvent);
     }
   }
